f3685c1d34c077e1845e26330ab76abcd14c369c
jibran-hussain-mir <84909335+jibran-hussain-mir@users.noreply.github.com>
2021-09-25 11:51:52 +0530

    Add files via upload

---
+++ Display only Even numbers in a linked list.cpp
@@ -0,0 +1,71 @@
+//Display Even Numbers in a linked list
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+	{
+		int data;
+		struct Node *next;
+	}*head=NULL;
+	
+int create()
+	{
+		int n;  	//No. of Nodes
+		printf("Enter the number of Nodes : ");
+		scanf("%d",&n);
+		printf("\n");
+		struct Node *temp,*tail;
+		for(int i=0;i<n;i++)	//Creating n number of nodes
+			{
+				temp=(struct Node *)malloc(sizeof(struct Node));
+				printf("Enter data for Node %d :",i+1);
+				scanf("%d",&temp->data);
+				temp->next=NULL;
+				if(head==NULL)
+					{
+						head=temp;
+						tail=temp;
+					}
+				else
+					{
+						tail->next=temp;
+						tail=temp;
+					}
+					
+			}
+			temp->next=NULL;
+			
+	}
+	
+void display(struct Node *p)	//DISPLAYING LINKED LIST
+	{
+		while(p!=NULL)
+			{
+				printf("%d ",p->data);
+				p=p->next;
+			}
+			printf("\n\n");
+	}
+
+void print_odd(struct Node *p)
+	{
+		printf("\n Even values from the above linked list are :\n");
+		while(p!=NULL)
+			{
+				if(p->data%2==0)
+					{
+						printf("%d ",p->data);
+					}
+					p=p->next;
+			}
+	}
+
+int main()
+	{
+		create();
+		display(head);
+		print_odd(head);
+	}
+
+	

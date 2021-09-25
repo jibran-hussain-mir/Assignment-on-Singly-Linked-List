485176cac230f7b7eb72f7bc72ced615841e6199
jibran-hussain-mir <84909335+jibran-hussain-mir@users.noreply.github.com>
2021-09-25 15:20:32 +0530

    Add files via upload

---
+++ Frequency of a given number in a linked list.cpp
@@ -0,0 +1,67 @@
+// Frequency of a given number in a linked list
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+	{
+		int data;
+		struct Node *next;
+	}*head=NULL;
+	
+int create()
+	{
+		int n;  	//No. of Nodes
+		printf("Enter the number of Nodes : ");
+		scanf("%d",&n);
+		printf("\n");
+		struct Node *temp,*tail;
+		for(int i=0;i<n;i++)	//Creating n number of nodes
+			{
+				temp=(struct Node *)malloc(sizeof(struct Node));
+				printf("Enter data for Node %d :",i+1);
+				scanf("%d",&temp->data);
+				temp->next=NULL;
+				if(head==NULL)
+					{
+						head=temp;
+						tail=temp;
+					}
+				else
+					{
+						tail->next=temp;
+						tail=temp;
+					}
+					
+			}
+			temp->next=NULL;
+			
+	}
+	void display(struct Node *p)	//DISPLAYING LINKED LIST
+	{
+		while(p!=NULL)
+			{
+				printf("%d ",p->data);
+				p=p->next;
+			}
+			printf("\n\n");
+	}
+	
+int frequency_of_element(struct Node *p,int value)
+	{
+		int count=0;
+		while(p!=NULL)
+			{
+				if(p->data==value) count++;
+				p=p->next;
+			}
+		return count;
+	}
+
+int main()
+	{
+		create();
+		display(head);
+		printf("Frequency = %d ",frequency_of_element(head,2));
+	}
+
